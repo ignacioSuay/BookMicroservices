7837084c5c733e9000cdd6fb26e030dc430ef10e
ignacio.suay <ignacio.suay@gmail.com>
2016-10-15 18:45:51 +0100

    create Book service

---
+++ BookMicroservice/src/main/java/com/ignaciosuay/bookmicroservices/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.ignaciosuay.bookmicroservices.config.audit;
---
+++ BookMicroservice/src/main/java/com/ignaciosuay/bookmicroservices/web/filter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Servlet filters.
+ */
+package com.ignaciosuay.bookmicroservices.web.filter;
